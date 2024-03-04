924c832cec6c8bdea1782246e02e621e207b3d79
Daniel from Phcity <103477809+CoderOfPHCity@users.noreply.github.com>
2024-03-04 13:36:03 +0100

    Update DegenToken.sol

--- contracts/DegenToken.sol
+++ contracts/DegenToken.sol
@@ -1,90 +1,70 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.18;
+pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "hardhat/console.sol";
 
-contract DegenToken is ERC20, Ownable {
-    mapping(address => mapping(address => uint256)) private _allowances;
-
+contract DegenToken is ERC20, Ownable, ERC20Burnable {
     constructor() ERC20("Degen", "DGN") {}
 
     function mint(address to, uint256 amount) public onlyOwner {
-        _mint(to, amount);
+        _mint(to, amount); // last value is for decimals
     }
 
-    function approve(address spender, uint256 amount)
-        public
-        virtual
-        override
-        returns (bool)
-    {
-        _approve(_msgSender(), spender, amount);
-        return true;
+    function decimals() override public pure returns (uint8) {
+        return 0;
     }
 
-    function allowance(address owner, address spender)
-        public
-        view
-        virtual
-        override
-        returns (uint256)
-    {
-        return _allowances[owner][spender];
+    function getBalance() external view returns (uint256) {
+        return this.balanceOf(msg.sender);
     }
 
-    function transfer(address to, uint256 amount)
-        public
-        virtual
-        override
-        returns (bool)
-    {
-        _chargeTransferFee(_msgSender(), amount);
-        _transfer(_msgSender(), to, amount);
-        return true;
+    function transferTokens(address _receiver, uint256 _value) external {
+        require(balanceOf(msg.sender) >= _value, "You do not have enough Degen Tokens");
+        approve(msg.sender, _value);
+        transferFrom(msg.sender, _receiver, _value);
     }
 
-    function transferFrom(
-        address sender,
-        address recipient,
-        uint256 amount
-    ) public virtual override returns (bool) {
-        _chargeTransferFee(sender, amount);
-        _transfer(sender, recipient, amount);
-        uint256 currentAllowance = _allowances[sender][_msgSender()];
-        require(
-            currentAllowance >= amount,
-            "ERC20: transfer amount exceeds allowance"
-        );
-        _approve(sender, _msgSender(), currentAllowance - amount);
-        return true;
+    function burnTokens(uint256 _value) external {
+        require(balanceOf(msg.sender) >= _value, "You do not have enough Degen Tokens");
+        burn(_value);
     }
 
-   function redeem(uint256 playerAmount) public returns (bool) {
-    require(playerAmount > 0, "Amount must be greater than 0");
-    
-    uint256 totalTokensRequired = playerAmount * 10; // 10% of the playerAmount
-
-    require(balanceOf(msg.sender) >= totalTokensRequired, "You do not have enough Degen Tokens");
-
-    // Transfer tokens from user to owner
-    approve(owner(), totalTokensRequired);
-    transferFrom(msg.sender, owner(), totalTokensRequired);
-
-    return true;
-}
-
-
-    function checkBalance(address account) public view returns (uint256) {
-        return balanceOf(account);
+    function showStoreItems() external view returns (string memory) {
+        console.log("The following items are available for purchase:");
+        console.log("Item 1. Official Degen NFT");
+        console.log("Item 2. Degen Sorts");
+        console.log("Item 3. Degen Master");
+        return "The following items are available for purchase:\nItem 1. Official Degen NFT\nItem 2. Official Degen NFT\nItem 3. Official Degen NFT";
     }
 
-    function burn(uint256 amount) public {
-        _burn(msg.sender, amount);
-    }
-
-    function _chargeTransferFee(address sender, uint256 amount) internal {
-        uint256 fee = (amount * 2) / 100; // 2% fee
-        _burn(sender, fee);
+    function redeemTokens(uint8 _Item) external payable returns (bool) {
+        if (_item == 1) {
+            require(this.balanceOf(msg.sender) >= 100, "You do not have enough Degen Tokens");
+            approve(msg.sender, 100);
+            transferFrom(msg.sender, owner(), 100);
+            console.log("You have redeemed for an official Degen NFT!");
+            return true;
+        }
+        else if (_Item == 2) {
+            require(this.balanceOf(msg.sender) >= 75, "You do not have enough Degen Tokens");
+            approve(msg.sender, 75);
+            transferFrom(msg.sender, owner(), 75);
+            console.log("You have redeemed for an official Degen Shorts!");
+            return true;
+        }
+        else if (_Item == 3) {
+            require(this.balanceOf(msg.sender) >= 50, "You do not have enough Degen Tokens");
+            approve(msg.sender, 50);
+            transferFrom(msg.sender, owner(), 50);
+            console.log("You have redeemed for master in our Degen Discord!");
+            return true;
+        }
+        else {
+            console.log("That is an invalid item");
+            return false;
+        }
     }
 }
